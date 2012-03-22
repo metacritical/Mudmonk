63af71ffe1dba71a9ab5e67f2421c80c04d49261
Pankaj Doharey <pankajdoharey@live.com>
2012-03-22 19:21:51 +0530

    More Handler bytes is confusing

--- controller/keyb_handler.rb
+++ controller/keyb_handler.rb
@@ -3,41 +3,43 @@ class KeyboardHandler
     @io = io
   end
 
-  def read_data
-    character = @io.bytes.each do |i|
-      i.to_s(16)
-    end
-  end
-
+#  def read_data
+#    multi_bytes = @io.bytes.each do |i|
+#      i.to_s(16)
+#    end
+#    return multi_bytes
+#  end
+#
   def run
     loop do
-      #character = @io.getbyte.to_s(16)
-      char_data = read_data
+      character = @io.getbyte.to_s(16)
+#      char_data = read_data
 
-      case char_data
+      case character
       when "1b" #<esc> Character
         @io.print "\x8\x8"
         @io.print "Shorel 'aran\n"
         $monkserver.stop
                 
-#      when "41" #"A"
-#        @io.print "\x8"
-#        @io.print "B"
-#        #io.print "\x1b\x238" #Repeat E on screen
-#        
-#      when "8" #"C-h" combination keypress
-#        @io.print "\x8\x8"
-#        @io.puts "Help commands : blah..blah.."
-#        @io.print "\n~> "
-#
-#      when "7f"
-#        @io.print "\x8\x8\x8"
-#        
-#      when "\e"
-#        @io.print "Da..Da..da.da..da"
-#        
+      when "41" #"A"
+        @io.print "\x8"
+        @io.print "B"
+        #io.print "\x1b\x238" #Repeat E on screen
+        
+      when "8" #"C-h" combination keypress
+        @io.print "\x8\x8"
+        @io.puts "Help commands : blah..blah.."
+        @io.print "\n~> "
+
+      when "7f"
+        @io.print "\x8\x8\x8"
+        
+      when "\e"
+        @io.print "Da..Da..da.da..da"
+        
       else
-      @io.print char_data
+     # multi_character = self.read_data
+      @io.print character
       end
     end
   end
