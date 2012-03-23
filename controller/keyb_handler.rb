8a9c21f18d1fb4e29a8b468de5ae5e46c498f11d
Pankaj Doharey <pankajdoharey@live.com>
2012-03-23 07:05:52 +0530

    Keys Added multi byte dosen't work but getsc works too

--- controller/keyb_handler.rb
+++ controller/keyb_handler.rb
@@ -3,43 +3,43 @@ class KeyboardHandler
     @io = io
   end
 
-#  def read_data
-#    multi_bytes = @io.bytes.each do |i|
-#      i.to_s(16)
-#    end
-#    return multi_bytes
-#  end
-#
+  def read_data
+    multi_bytes = @io.bytes.each do |i|
+      i.to_s(16)
+    end
+  end
+
   def run
     loop do
-      character = @io.getbyte.to_s(16)
-#      char_data = read_data
+      character = @io.getbytes.to_s(16)
+      #multi_bytes = @io.bytes.each {|i| i.to_s(16) }
 
-      case character
-      when "1b" #<esc> Character
-        @io.print "\x8\x8"
-        @io.print "Shorel 'aran\n"
-        $monkserver.stop
-                
-      when "41" #"A"
-        @io.print "\x8"
-        @io.print "B"
-        #io.print "\x1b\x238" #Repeat E on screen
-        
-      when "8" #"C-h" combination keypress
-        @io.print "\x8\x8"
-        @io.puts "Help commands : blah..blah.."
-        @io.print "\n~> "
-
-      when "7f"
-        @io.print "\x8\x8\x8"
-        
-      when "\e"
-        @io.print "Da..Da..da.da..da"
-        
+    case character
+     when "11" #C-q KeyCombo
+     @io.print "\x8\x8"
+     @io.print "\nShorel 'aran\n"
+     $monkserver.stop
+               
+     when "41" #"A" Character
+       @io.print "\x8"
+       @io.print "B"
+       #@io.print "\x1b\x238" #Repeat E on screen
+       
+     when "8" #"C-h" combination keypress
+       @io.print "\x8\x8"
+       @io.puts "Help commands : blah..blah.."
+       @io.print "\n~> "
+   
+     when "7f" #Backspace
+       @io.print "\x8\x8\x8"
+       
       else
-     # multi_character = self.read_data
-      @io.print character
+      #character = @io.bytes.each {|i| i.to_s(16) }
+      
+      #case multi_character
+      #  when "1b5b337e"
+      #@io.print read_data
+      @io.print character + "-"
       end
     end
   end
